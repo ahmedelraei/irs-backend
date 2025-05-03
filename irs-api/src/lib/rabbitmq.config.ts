d1b161e1b1aaa74b8471182e45ae6784b59fe516
Ahmed Hatem <ahmedelrai18@gmail.com>
2025-05-03 06:32:59 +0300

    feat: update RabbitMQ configuration to use environment variable for URL and add it to deployment environment variables

--- irs-api/src/lib/rabbitmq.config.ts
+++ irs-api/src/lib/rabbitmq.config.ts
@@ -1,12 +1,14 @@
 import { Transport, ClientsModuleOptions } from '@nestjs/microservices';
 
+const url = process.env.RABBITMQ_URL || 'amqp://guest:guest@localhost:5672';
+
 export const rabbitMQConfig: ClientsModuleOptions = {
   clients: [
     {
       name: 'RABBITMQ_ML_PRODUCER_SERVICE',
       transport: Transport.RMQ,
       options: {
-        urls: ['amqp://guest:guest@localhost:5672'],
+        urls: [url],
         queue: 'texts_queue',
         queueOptions: {
           durable: false,
@@ -17,7 +19,7 @@ export const rabbitMQConfig: ClientsModuleOptions = {
       name: 'RABBITMQ_ML_CONSUMER_SERVICE',
       transport: Transport.RMQ,
       options: {
-        urls: ['amqp://guest:guest@localhost:5672'],
+        urls: [url],
         queue: 'text_embeddings',
         queueOptions: {
           durable: false,
@@ -28,7 +30,7 @@ export const rabbitMQConfig: ClientsModuleOptions = {
       name: 'RABBITMQ_ML_JOB_PRODUCER_SERVICE',
       transport: Transport.RMQ,
       options: {
-        urls: ['amqp://guest:guest@localhost:5672'],
+        urls: [url],
         queue: 'job_texts_queue',
         queueOptions: {
           durable: false,
@@ -39,7 +41,7 @@ export const rabbitMQConfig: ClientsModuleOptions = {
       name: 'RABBITMQ_ML_JOB_CONSUMER_SERVICE',
       transport: Transport.RMQ,
       options: {
-        urls: ['amqp://guest:guest@localhost:5672'],
+        urls: [url],
         queue: 'job_texts_embeddings',
         queueOptions: {
           durable: false,
@@ -50,7 +52,7 @@ export const rabbitMQConfig: ClientsModuleOptions = {
       name: 'RABBITMQ_JOB_PRODUCER_SERVICE',
       transport: Transport.RMQ,
       options: {
-        urls: ['amqp://guest:guest@localhost:5672'],
+        urls: [url],
         queue: 'job_queue',
         queueOptions: {
           durable: false,
@@ -61,7 +63,7 @@ export const rabbitMQConfig: ClientsModuleOptions = {
       name: 'RABBITMQ_JOB_CONSUMER_SERVICE',
       transport: Transport.RMQ,
       options: {
-        urls: ['amqp://guest:guest@localhost:5672'],
+        urls: [url],
         queue: 'scraped_job_queue',
         queueOptions: {
           durable: false,
